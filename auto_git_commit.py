8723f9e3442ca7916aa476cbd0d2cc941c52fd6d
tingjianlau <tingjianlau@gmail.com>
2016-06-15 23:17:59 +0800

    将本地项目提交到github中.txt

---
+++ auto_git_commit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+##########################################################################
+#	> File Name: git_auto_commit.py
+#	> Author: Tingjian Lau
+#	> Mail: tjliu@mail.ustc.edu.cn
+#	> Created Time: 2016/05/15
+#########################################################################
+
+import os, sys
+from os import system
+
+if __name__ == '__main__':
+    msg = sys.argv[1]
+
+    #command = 'python auto_create_readme.py'
+    #system(command)
+    
+    command = 'git add .'
+    system(command)
+
+    command = 'git commit -m \"' + msg + '\"'
+    system(command)
+
+    command = 'git push origin master'
+    system(command)
